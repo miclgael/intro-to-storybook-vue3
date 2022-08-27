0ac7c509e62f65d9e9a942769ec5cff5297b6275
Michael Gale <michael@michaelgale.dev>
2022-08-27 10:51:40 +1000

    feat: add task component and its initial stories

---
+++ src/components/SingleTask.stories.js
@@ -0,0 +1,52 @@
+import SingleTask from './SingleTask.vue';
+
+import { action } from '@storybook/addon-actions';
+
+export default {
+  component: SingleTask,
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  title: 'Atoms/SingleTask',
+  //👇 Our events will be mapped in Storybook UI
+  argTypes: {
+    onPinTask: {},
+    onArchiveTask: {},
+  },
+};
+
+export const actionsData = {
+  onPinTask: action('pin-task'),
+  onArchiveTask: action('archive-task'),
+};
+
+const Template = args => ({
+  components: { SingleTask },
+  setup () {
+    return { args, ...actionsData };
+  },
+  template: '<SingleTask v-bind="args" />',
+});
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
