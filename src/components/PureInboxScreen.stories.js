c0344b8af4f153b849a06f37c0562c3be3da6153
Michael Gale <michael@michaelgale.dev>
2022-09-02 14:27:46 +1000

    feat: add screen interactions

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,6 +1,6 @@
 import { app } from '@storybook/vue3';
-
 import { createStore } from 'vuex';
+import { fireEvent, within } from '@storybook/testing-library';
 
 import PureInboxScreen from './PureInboxScreen.vue';
 import { action } from '@storybook/addon-actions';
@@ -32,7 +32,7 @@ const store = createStore({
 app.use(store);
 
 export default {
-  title: 'PureInboxScreen',
+  title: 'Organisms/PureInboxScreen',
   component: PureInboxScreen,
 };
 
@@ -50,3 +50,13 @@ export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = { error: true };
+
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+};
